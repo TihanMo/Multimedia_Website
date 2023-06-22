8aab8708d396d233ff61eeead0a88550925f1dd0
TihanMo <tihan.morrol@lernende.bbw.ch>
2023-06-22 19:54:13 +0200

    parralax image added (not working)

--- React_App/src/App.jsx
+++ React_App/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from '../src/components/Navbar'
+import Home from './components/Home'
 
 function App() {
 
   return (
     <>
       <Navbar position="static" className="navbar" />
+      <Home/>
     </>
   )
 }
